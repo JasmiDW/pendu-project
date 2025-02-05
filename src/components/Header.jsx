16e5f9f7d847603835a036789646ac52df454b8c
Nadia Rousseau <nadia.rousseau@serli.com>
2025-02-05 15:32:53 +0100

    feat(section header)

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <header>
+            <h1> Assembly : Endgame</h1>
+            <p className="p-header">Devine le mot en 8 essais pour sauver le monde de la programmation de l'Assembly !</p>
+        </header>
+    )
+}
