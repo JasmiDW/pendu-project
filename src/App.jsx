a41d28c3f18e69d84b7a1e65f5abec7b6aa2f288
Nadia Rousseau <nadia.rousseau@serli.com>
2025-02-05 16:34:22 +0100

    style(update css)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
 import Header from './components/Header'
+import { languages } from './languages'
 
 function App() {
- 
+  const languageElements = languages.map(lang => {
+    const styles = {
+        backgroundColor: lang.backgroundColor,
+        color: lang.color
+    }
+    return (
+        <span 
+            className="span-languages" 
+            style={styles}
+            key={lang.name}
+        >
+            {lang.name}
+        </span>
+    )
+})
+
   return (
     <main>
       <Header />
+      <section className='status-game'>
+        <div>
+          <h2>You win !</h2>
+          <p>Well done</p>
+        </div>
+      </section>
+      <section className="language-chips">
+      {languageElements}
+      </section>
+      
     </main>
   )
 }
