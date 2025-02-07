071d7f4e261b1fbbf32e01e3dcec35d8c2d2d3d5
Nadia Rousseau <nadia.rousseau@serli.com>
2025-02-07 10:21:50 +0100

    feat(wrong guess count)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ import { languages } from './languages'
 import { useState } from 'react'
 
 function App() {
+  
+  const [currentWord, setCurrentWord] = useState("react")
+  const [guessedLetters, setGuessedLetters] = useState([])
+
+  const wrongGuessCount = guessedLetters.filter(letter => !currentWord.includes(letter)).length
+  console.log(wrongGuessCount)
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  
+  function handleLetter(letter){
+    setGuessedLetters(prevLetter => prevLetter.includes(letter) ? prevLetter : [...prevLetter, letter])
+  }
+
   const languageElements = languages.map(lang => {
     const styles = {
         backgroundColor: lang.backgroundColor,
@@ -18,56 +31,47 @@ function App() {
             {lang.name}
         </span>
     )
-})
+  })
 
-const [currentWord, setCurrentWord] = useState("react")
-const [guessedLetters, setGuessedLetters] = useState([])
+  const lettersArray = currentWord.split('')
+  const lettersLang = lettersArray.map((letter, index) => {
+    return (<span  className="span-letters" key={index}>{guessedLetters.includes(letter) ? letter : ""}</span>)
+  })
 
-const lettersArray = currentWord.split('')
+  const alphArray = alphabet.split('')
+  const keyboard= alphArray.map(item => {
 
-const lettersLang = lettersArray.map((letter, index) => {
-  return (<span className="span-letters" key={index}>{letter}</span>)
-})
+    const isGuessed = guessedLetters.includes(item)
+    const isCorrect = isGuessed && currentWord.includes(item)
+    const isWrong = isGuessed && !currentWord.includes(item)
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz"
-const alphArray = alphabet.split('')
-const keyboard= alphArray.map(item => {
-  
-  const isGuessed = guessedLetters.includes(item)
-  const isCorrect = isGuessed && currentWord.includes(item)
-  const isWrong = isGuessed && !currentWord.includes(item)
+    return (
+      <button className={`
+        ${isCorrect ? "bg-green" : ""}
+        ${isWrong ? "bg-red" : ""}
+        ${!isGuessed ? "" : ""}`} onClick={() => handleLetter(item)} key={item}>{item}</button>
+    )
+  })
 
-  return (
-    <button className={`
-      ${isCorrect ? "bg-green" : ""}
-      ${isWrong ? "bg-red" : ""}
-      ${!isGuessed ? "" : ""}`} onClick={() => handleLetter(item)} key={item}>{item}</button>
-  )
-})
+    return (
+      <main>
+        <Header />
+        <section className='status-game'>
+            <h2>You win !</h2>
+            <p>Well done</p>
+        </section>
+        <section className="language-chips">
+        {languageElements}
+        </section>
+        <section className="word">
+          {lettersLang}
+        </section>
+        <section className="keyboard">
+          {keyboard}
+        </section>
 
-function handleLetter(letter){
-  setGuessedLetters(prevLetter => prevLetter.includes(letter) ? prevLetter : [...prevLetter, letter])
-}
-
-  return (
-    <main>
-      <Header />
-      <section className='status-game'>
-          <h2>You win !</h2>
-          <p>Well done</p>
-      </section>
-      <section className="language-chips">
-      {languageElements}
-      </section>
-      <section className="word">
-        {lettersLang}
-      </section>
-      <section className="keyboard">
-        {keyboard}
-      </section>
-
-    </main>
-  )
+      </main>
+    )
 }
 
 export default App
